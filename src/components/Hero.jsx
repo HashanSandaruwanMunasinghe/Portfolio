93d3899aca3f797c340a0dfcf3c4c62e1153b6d8
HashanSandaruwanMunasinghe <munasinghefirst@gmail.com>
2025-05-27 21:24:29 +0530

    move2

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -64,7 +64,7 @@ const Hero = () => {
               <div className="absolute inset-0 rounded-full bg-blue-600/10 animate-pulse"></div>
               <div className="absolute inset-2 rounded-full bg-blue-600/20"></div>
               <div className="absolute inset-4 rounded-full overflow-hidden border-4 border-white shadow-lg">
-                <img src="/home.jpeg" alt="Home" />
+                <img src="public/home.jpeg" alt="Home" />
 
               </div>
             </div>
